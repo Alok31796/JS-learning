9af40574eb88c08bb2e76643d9fd73266f85ef36
Alok31796 <38124828+Alok31796@users.noreply.github.com>
2025-05-16 06:47:57 +0000

    conversion of datatypes done

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,48 @@
+console.log("03_conversionOperators");
+
+// let score = 33;
+// let score = "33";
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = false;
+// let score = "ALOKPandey";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33;
+// "33abc" => Nan; (not a number)   but type number aata h
+// true => 1; false => 0;
+
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "" // false
+// let isLoggedIn = " " // true
+// let isLoggedIn = "alok" // true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false; 
+//  "" => false
+//  "alok" => true
+
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
